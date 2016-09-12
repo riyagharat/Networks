f912f1bb52ba7f745ac223c51ec55faf42af81ca
Riya Gharat <riya.gharat@yahoo.com>
2016-09-12 13:37:36 -0400

    Update 1 to Project1.java
    Added the basic structure for a menu

--- Project1.java
+++ Project1.java
@@ -9,6 +9,45 @@ Mai Nguyen
 
 class Project1{
    public static void main(String[]args){
+      boolean check = true;
+      while(check){
+         System.out.println("Option a: Host Current Date and Time");
+         System.out.println("Option b: Host Uptime");
+         System.out.println("Option c: Host Memory Use");
+         System.out.println("Option d: Host Netstat");
+         System.out.println("Option e: Host Current Users");
+         System.out.println("Option f: Host Running Processes");
+         System.out.println("Option g: EXIT.");
+         System.out.print("Enter a choice: ");
+         choice = input.next().charAt(0);
+         System.out.println("");
+         
+         switch(choice){
+            case 'a':
+               
+              break;
+            case 'b':
+                 
+              break;
+            case 'c':
+                
+              break;
+            case 'd':
+                
+              break;
+            case 'e':
+                 
+              break;
+            case 'f':
+                 
+              break; 
+            case 'g':
+             
+              break;
+              default:
+                 System.out.print("Invalid entry\n");
+           }
+        }   
    
    }  
 }
