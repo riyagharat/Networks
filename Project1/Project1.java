01aabf322b57127e2a40c5b36b1a26421ff7b144
Riya Gharat <riya.gharat@yahoo.com>
2016-09-17 15:11:28 -0400

    Attempted some server side things
    They need to be modified in order to make it work though.

--- Project1/Project1.java
+++ Project1/Project1.java
@@ -9,6 +9,7 @@ Mai Nguyen              n01069097
 
 import java.util.*;
 import java.io.*;
+import java.net.*;
 
 class Project1{
    public static void main(String[] args){
@@ -16,51 +17,82 @@ class Project1{
       char choice; 
       Scanner input = new Scanner(System.in);//Creates a scanner for input value
       
-      String hostName = args[0];
-      if(hostName == null){
-         System.out.println("ERROR: Did not enter a hostname. The program will exit.");
-          System.exit(0); //Exits upon non-existent argument
-      }else{
-         boolean check = true;
-         while(check){
-            System.out.println("Option a: Host Current Date and Time");
-            System.out.println("Option b: Host Uptime");
-            System.out.println("Option c: Host Memory Use");
-            System.out.println("Option d: Host Netstat");
-            System.out.println("Option e: Host Current Users");
-            System.out.println("Option f: Host Running Processes");
-            System.out.println("Option g: EXIT.");
-            System.out.print("Enter a choice: ");
-            choice = input.next().charAt(0);
-            System.out.println("");
-            
-            switch(choice){
-               case 'a':
-                  
-                  
-                 break;
-               case 'b':
-                    
-                 break;
-               case 'c':
-                   
-                 break;
-               case 'd':
-                   
-                 break;
-               case 'e':
-                    
-                 break;
-               case 'f':
-                    
-                 break; 
-               case 'g':
-                System.exit(0);
-                 break;
-                 default:
-                    System.out.print("Invalid entry\n");
-              }
-           }   
+      if (args.length != 1) {
+            System.err.println("Usage: java server <port number>");
+            System.exit(1);
+        }
+ 
+        int portNumber = Integer.parseInt(args[0]);
+ 
+        try ( 
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out =
+                new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+        ){
+         
+            String inputLine, outputLine;
+             
+            // Initiate conversation with client
+            KnockKnockProtocol kkp = new KnockKnockProtocol();
+            outputLine = kkp.processInput(null);
+            out.println(outputLine);
+ 
+            while ((inputLine = in.readLine()) != null) {
+                outputLine = kkp.processInput(inputLine);
+                out.println(outputLine);
+                if (outputLine.equals("Bye."))
+                    break;
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
+    }
+    
+      boolean check = true;
+      while(check){
+         System.out.println("Option a: Host Current Date and Time");
+         System.out.println("Option b: Host Uptime");
+         System.out.println("Option c: Host Memory Use");
+         System.out.println("Option d: Host Netstat");
+         System.out.println("Option e: Host Current Users");
+         System.out.println("Option f: Host Running Processes");
+         System.out.println("Option g: EXIT.");
+         System.out.print("Enter a choice: ");
+         choice = input.next().charAt(0);
+         System.out.println("");
+         
+         switch(choice){
+            case 'a':
+               
+               
+              break;
+            case 'b':
+                 
+              break;
+            case 'c':
+                
+              break;
+            case 'd':
+                
+              break;
+            case 'e':
+                 
+              break;
+            case 'f':
+                 
+              break; 
+            case 'g':
+             System.exit(0);
+              break;
+              default:
+                 System.out.print("Invalid entry\n");
+           }
+        }   
    
    }  
 }
