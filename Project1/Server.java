c12747fcea765df09d42fb87d0a9a915fdb20a71
meganmolumby <megan.gandy@hotmail.com>
2016-09-29 11:38:27 -0400

    Create Server.java

---
+++ Project1/Server.java
@@ -0,0 +1,37 @@
+import java.net.*;
+
+public class Server {
+  public static void main(String args[]) {
+
+    ServerSocket echoServer = null;
+    String line;
+    DataInputStream is;
+    PrintStream os;
+    Socket clientSocket = null;
+
+    /*
+     * Open a server socket on port 2222. Note that we can't choose a port less
+     * than 1023 if we are not privileged users (root).
+     */
+    try {
+      echoServer = new ServerSocket(2222);
+    } catch (IOException e) {
+      System.out.println(e);
+    }
+
+    /*
+     * Create a socket object from the ServerSocket to listen to and accept
+     * connections. Open input and output streams.
+     */
+    System.out.println("The server started. To stop it press <CTRL><C>.");
+    try {
+      clientSocket = echoServer.accept();
+      is = new DataInputStream(clientSocket.getInputStream());
+      os = new PrintStream(clientSocket.getOutputStream());
+
+      }
+    } catch (IOException e) {
+      System.out.println(e);
+    }
+  }
+}
