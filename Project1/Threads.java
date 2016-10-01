b2b01491aebfa73b24aee40aba67c3e7a7497805
Riya Gharat <riya.gharat@yahoo.com>
2016-10-01 16:48:22 -0400

    Update Threads.java

--- Project1/Threads.java
+++ Project1/Threads.java
@@ -1,27 +1,16 @@
 import java.net.*;
 import java.io.*;
 
-public class Threads {
+public class Threads implements Runnable{
+   static int maxClients = 5;
    public static void main(String[] args) {
-      int maxClients = 5;
+      Threads t = new Threads();
       for(int i = 0; i < maxClients; i++){
-         Thread thread = new Loops("Hello");
-         for(int j = 0; j < maxClients; j++){
-            thread.start();
-         }
+         new Thread(t).start();
       }
-   }
-}
-
-class Loops extends Thread {
-   String message;
-   
-   Loops(String mess) {
-      message = mess; 
-   }
-   public void run() {
-      while(true) {
-         System.out.println(message);
-      }
+   }   
+      
+   public void run(){
+    
    }
 }
