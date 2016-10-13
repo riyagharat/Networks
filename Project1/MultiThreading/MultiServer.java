2327a0f918d0edabcee0ce546c848c18bf197a64
meganmolumby <megan.gandy@hotmail.com>
2016-10-13 08:53:55 -0400

    Update MultiServer.java

--- Project1/MultiThreading/MultiServer.java
+++ Project1/MultiThreading/MultiServer.java
@@ -1,4 +1,4 @@
-/*
+/* CLIENT MULTI THREADING
 Riya Gharat             n00901846
 Hame Britto             n00938821
 Megan Molumby           n00942101
@@ -9,74 +9,112 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-public class MultiServer {
- 
-  public static void main(String args[]) {
-    String TimeStamp;
-    ServerSocket echoServer = null;
-    String line;
-    DataInputStream is;
-    PrintStream os;
-    Socket clientSocket = null;
-    String netStat;
-    
-    char choice;
+public class MultiClient extends Thread{
+  public static String variable = "";
+  
+  public static void main(String[] args) {
+      while(true){
+         variable = menu();
+         System.out.println(variable);
+         createThreads();
+      }
+  }
+  
+  public static void createThreads(){
+      for(int i = 0; i < 5; i++){
+            new Thread(new ClientThread("Thread" + i)).start();
+      }
+  }
+  
+  public static String menu(){
+      boolean check = true;
+      String choice;
+      Scanner input = new Scanner(System.in);
+
+      while(check){
+            System.out.println("Option a: Host Current Date and Time");
+            System.out.println("Option d: Host Netstat"); 
+            System.out.println("Option g: Exit Program");
+            System.out.print("Enter a choice: ");
+            choice = input.next();
+            System.out.println("");
+           switch(choice){
+              case "a":
+                  return "a";      
+              case "d":
+                return "d";
+              case "g":
+               System.exit(0);
+                break;
+              default:
+                 System.out.print("Invalid entry\n");
+              }
+           } 
+      return "END";
+  }
+  
+} 
+
+class ClientThread implements Runnable{
+   String threadName;
+
+   public ClientThread(String name){
+      threadName = name;
+   }   
    
+   public void run(){
+   
+      System.out.println("Process started for" + threadName);
+      Socket clientSocket = null;
+      PrintStream os = null;
+      DataInputStream is = null;
+      DataInputStream inputLine = null;
 
     /*
-     * Open a server socket on port 2222. Note that we can't choose a port less
-     * than 1023 if we are not privileged users (root).
+     * Open a socket on port 2222 of the server to the virtual machine ciswkstn113
      */
     try {
-      echoServer = new ServerSocket(2222);
+      clientSocket = new Socket("192.168.100.113", 2222);
+      os = new PrintStream(clientSocket.getOutputStream());
+      is = new DataInputStream(clientSocket.getInputStream());
+      inputLine = new DataInputStream(new BufferedInputStream(System.in));
+      
+    } catch (UnknownHostException e) {
+      System.err.println("Don't know about host");
     } catch (IOException e) {
-      System.out.println(e);
+      System.err.println("Couldn't get I/O for the connection to host");
     }
 
     /*
-     * Create a socket object from the ServerSocket to listen to and accept
-     * connections. Open input and output streams.
+     * If everything has been initialized then we want to write some data to the
+     * socket we have opened a connection to on port 2222.
      */
-    System.out.println("The server started. To stop it press <CTRL><C>.");
     
-        try {
-      clientSocket = echoServer.accept();
-      
-      is = new DataInputStream(clientSocket.getInputStream());
-      os = new PrintStream(clientSocket.getOutputStream());
-      
-      
-//while loop looks for user menu choice
-      while(true){
-         line= is.readLine();
-         String s = "";         
-         
-               switch(line){
-                  case "a":
-                  TimeStamp = new java.util.Date().toString();
-                  String returnTime ="Server responded at" + TimeStamp ;
-                   System.out.println(returnTime);
-                     os.flush();
-                           break;
+    
+       if (clientSocket != null && os != null && is != null) {
+         try {
+   
+           System.out.println("The client started. Type any text. To quit it type 'Ok'.");
+           
+           String responseLine = MultiClient.variable; 
+           System.out.println(MultiClient.variable);
+           
+           os.println(MultiClient.variable);
                  
-                 case "d":
-                     try{
-                        Process process = Runtime.getRuntime().exec("netstat");
-                        BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                        while ((s = stdInput.readLine()) != null) {
-                           System.out.println(s);
-                        }  
-                     }catch(IOException e){
-                     }       
-                     os.flush();              
-                        break;
-                 case "g":
-      
-               }
-      }
-     
-    } catch (IOException e) {
-      System.out.println(e);
-    }
-  }
+           /*
+            * Close the output stream, close the input stream, close the socket.
+            */
+           os.close();
+           is.close();
+           clientSocket.close();
+         } catch (UnknownHostException e) {
+           System.err.println("Trying to connect to unknown host: " + e);
+         } catch (IOException e) {
+           System.err.println("IOException:  " + e);
+         }
+       }
+       
+    System.out.println("Closing Socket.");
+}
+
 }
